10e1345d1eee4c0eb756d870fc02163c3881baf5
quid-0 <wodnd101@naver.com>
2023-08-26 15:56:08 +0900

    init

---
+++ src/test/kotlin/com/quid/wms/WmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quid.wms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WmsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
