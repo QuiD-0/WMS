c27bb3e32486e2d10ccb5c34b805e81b96d528ab
quid-0 <wodnd101@naver.com>
2023-08-28 22:19:45 +0900

    create ProductRepository

--- src/test/kotlin/com/quid/wms/product/RegisterProductTest.kt
+++ src/test/kotlin/com/quid/wms/product/RegisterProductTest.kt
@@ -1,5 +1,6 @@
 package com.quid.wms.product
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -7,10 +8,12 @@ import org.junit.jupiter.api.Test
 class RegisterProductTest {
 
     private lateinit var registerProduct: RegisterProduct
+    private lateinit var productRepository: ProductRepository
 
     @BeforeEach
     fun setUp() {
-        registerProduct = RegisterProduct()
+        productRepository = ProductRepository()
+        registerProduct = RegisterProduct(productRepository)
     }
 
     @Test
@@ -32,13 +35,16 @@ class RegisterProductTest {
         )
         registerProduct.request(request)
 
-//        assertThat(productRepository.findAll()).hasSize(1)
+        assertThat(productRepository.findAll()).hasSize(1)
     }
 }
 
-class RegisterProduct {
+class RegisterProduct(
+    private val productRepository: ProductRepository
+) {
     fun request(request: RegisterProductRequest) {
-        println(request)
+        val product = request.toDomain()
+        productRepository.save(product)
     }
 }
 
@@ -55,8 +61,26 @@ data class RegisterProductRequest(
     val widthInMillimeters: Long,
     val heightInMillimeters: Long,
     val lengthInMillimeters: Long,
+) {
+    init {
+        require(name.isNotBlank()) { "상품명은 필수입니다." }
+        require(code.isNotBlank()) { "상품 코드는 필수입니다." }
+        require(description.isNotBlank()) { "상품 설명은 필수입니다." }
+        require(brand.isNotBlank()) { "상품 브랜드는 필수입니다." }
+        require(maker.isNotBlank()) { "상품 제조사는 필수입니다." }
+        require(origin.isNotBlank()) { "상품 원산지는 필수입니다." }
+        require(weight > 0) { "상품 무게는 0보다 커야합니다." }
+        require(widthInMillimeters > 0) { "상품 가로 길이는 0보다 커야합니다." }
+        require(heightInMillimeters > 0) { "상품 세로 길이는 0보다 커야합니다." }
+        require(lengthInMillimeters > 0) { "상품 세로 길이는 0보다 커야합니다." }
+    }
 
+    fun toDomain() = Product(
+        1L, name, code, description, brand, maker, origin, category, temperatureZone, weight, ProductSize(
+            widthInMillimeters, heightInMillimeters, lengthInMillimeters
+        )
     )
+}
 
 enum class Category(
     val description: String
@@ -70,3 +94,34 @@ enum class TemperatureZone(
     ROOM_TEMPERATURE("상온"),
 }
 
+data class Product(
+    val id: Long,
+    val name: String,
+    val code: String,
+    val description: String,
+    val brand: String,
+    val maker: String,
+    val origin: String,
+    val category: Category,
+    val temperatureZone: TemperatureZone,
+    val weight: Long,
+    val size: ProductSize,
+)
+
+data class ProductSize(
+    val widthInMillimeters: Long,
+    val heightInMillimeters: Long,
+    val lengthInMillimeters: Long,
+)
+
+class ProductRepository {
+    private val products = mutableMapOf<Long, Product>()
+
+    fun findAll(): List<Product> {
+        return products.values.toList()
+    }
+
+    fun save(product: Product) {
+        products[product.id] = product
+    }
+}
