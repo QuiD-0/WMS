281a1f7280307974caf208fe8129d88bc108074c
QuiD-0 <wodnd101@naver.com>
2023-09-01 17:20:32 +0900

    type 종류 추가

--- src/main/kotlin/com/quid/wms/product/domain/Category.kt
+++ src/main/kotlin/com/quid/wms/product/domain/Category.kt
@@ -3,5 +3,9 @@ package com.quid.wms.product.domain
 enum class Category(
     val description: String
 ) {
-    ELECTRONICS("전자 제품")
+    ELECTRONICS("전자 제품"),
+    FOOD("식품"),
+    CLOTHES("의류"),
+    COSMETICS("화장품"),
+    BOOKS("도서"),
 }
--- src/main/kotlin/com/quid/wms/product/domain/TemperatureZone.kt
+++ src/main/kotlin/com/quid/wms/product/domain/TemperatureZone.kt
@@ -4,4 +4,6 @@ enum class TemperatureZone(
     val description: String
 ) {
     ROOM_TEMPERATURE("상온"),
+    COLD_TEMPERATURE("냉장"),
+    FROZEN_TEMPERATURE("냉동"),
 }
