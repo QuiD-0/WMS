2d45c6865395afcb3096fe4bcc7062b2bd2682be
quid-0 <wodnd101@naver.com>
2023-09-24 14:14:55 +0900

    읽기전용 트랜잭션 생성

--- src/main/kotlin/com/quid/wms/order/usecase/FindOrder.kt
+++ src/main/kotlin/com/quid/wms/order/usecase/FindOrder.kt
@@ -3,11 +3,13 @@ package com.quid.wms.order.usecase
 import com.quid.wms.order.domain.Order
 import com.quid.wms.order.gateway.repository.OrderRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 interface FindOrder {
     fun all(): List<Order>
 
     @Service
+    @Transactional(readOnly = true)
     class FindOrderImpl(
         private val orderRepository: OrderRepository,
     ) : FindOrder {
